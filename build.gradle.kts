1bc7b4ff06858c91adf4935e1ed6c098a317fcd2
neziw <kolopajlopiotr@gmail.com>
2024-10-06 18:26:08 +0200

    fix missing checkstyle in gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("checkstyle")
 }
 
 group = "ovh.neziw"
@@ -11,6 +12,11 @@ java {
 	toolchain {
 		languageVersion = JavaLanguageVersion.of(17)
 	}
+}
+
+checkstyle {
+	toolVersion = "10.18.2"
+	maxWarnings = 0
 }
 
 repositories {
